43db9891a17407b8bb7ab132251cdaad5055a8b2
200~Niraj-Kumar-007 <niraj25aeccse@gmail.com>
2025-08-09 14:22:03 +0530

    adding two numbers

---
+++ printing.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class printing{
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 2 values");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a+b;
+        System.out.println("Sum of a+b is = " + c);
+    }
+
+
+
+    }
+
